cd02ffb9b1334968883fbef3c1244d445b8aa4bf
luckyowl11 <sudhanshuseenver3.0@gmail.com>
2023-06-01 11:26:46 +0530

    render bacnkend api added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
 // const apiEndPoint = "http://localhost:8082/v1";
-const apiEndPoint = "https://yellowcontacts-production.up.railway.app/v1";
+// const apiEndPoint = "https://yellowcontacts-production.up.railway.app/v1";
+const apiEndPoint = "https://yellowcontacts.onrender.com/v1";
 
 export { apiEndPoint };
